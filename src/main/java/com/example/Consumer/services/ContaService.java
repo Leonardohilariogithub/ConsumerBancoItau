2104ea5fc9d88542bc6ae2b425cd21b648bbf0e9
Leonardo <66699109+Leonardohilariogithub@users.noreply.github.com>
2022-03-30 18:29:18 -0300

    primeiro commt do consumer banco itau

---
+++ src/main/java/com/example/Consumer/services/ContaService.java
@@ -0,0 +1,35 @@
+package com.example.Consumer.services;
+
+import com.example.Consumer.entidades.ContaEntidade;
+import com.example.Consumer.entidades.OperacaoEntidade;
+import org.springframework.stereotype.Service;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+@Service
+public class ContaService {
+
+    private final JedisPool pool = new JedisPool(new JedisPoolConfig(), "localhost",6379);
+
+    public void criarSaquesGratuitosNoRedis(ContaEntidade contaEntidade) {
+
+        Jedis jedis = pool.getResource();
+        jedis.set(contaEntidade.getNumeroDaConta(), Integer.toString(contaEntidade.getSaqueSemTaxa()));
+
+    }
+
+    public void atualizarSaquesGratuitosNoRedis(OperacaoEntidade operacaoEntidade) {
+
+        Jedis jedis = pool.getResource();
+
+        Integer saqueGratuitos = Integer.valueOf((jedis.get(operacaoEntidade.getNumeroDaConta())));
+
+        if(saqueGratuitos != 0){
+            jedis.set(operacaoEntidade.getNumeroDaConta(), Integer.toString(saqueGratuitos -1));
+        }
+
+
+
+    }
+}
